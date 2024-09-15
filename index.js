7413a967f0d03277036c3d1c49e3c36f0fdd6a3e
Seth Way <way.sethaniel@gmail.com>
2024-09-15 12:32:30 -0400

    modify puzzle format

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config({ path: '.env.local' });
 const express = require('express');
 const apiRouter = require('./routes/index.js');
+const cors = require('cors');
 const path = require('path');
 const { logErrors, clientErrorHandler, errorHandler } = require('./error.js');
 
 const app = express();
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
