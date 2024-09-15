e01ff25e037b8d8233acd6164bf6ddd4e75c7e02
Seth Way <way.sethaniel@gmail.com>
2024-09-15 13:01:35 -0400

    refactor for vercel

--- server.js
+++ server.js
@@ -24,21 +24,4 @@ const server = app.listen(PORT, () => {
   console.log(`Server is running on <><><> port ${PORT}`);
 });
 
-server.on('error', err => {
-  if (err.code === 'EADDRINUSE') {
-    console.error(`Port ${PORT} is already in use <><><> retrying...`);
-    server.close(err => {
-      if (err) console.error(err);
-      else {
-        setTimeout(() => {
-          server.listen(PORT);
-        }, 300);
-      }
-    });
-  } else {
-    console.error('Server error:', err);
-    process.exit(1);
-  }
-});
-
 module.exports = app;
