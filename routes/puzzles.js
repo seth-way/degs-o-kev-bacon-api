a2f069c1993324e75b807d02de2e37f3585a90ce
Seth Way <way.sethaniel@gmail.com>
2024-09-18 12:02:08 -0400

    testing node env

--- routes/puzzles.js
+++ routes/puzzles.js
@@ -3,69 +3,69 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-async function readFiles(filePaths) {
-  // Use map to create an array of promises that read the files
-  const filePromises = filePaths.map(async path => {
-    const data = await fs.promises.readFile(path, 'utf-8'); // Read the file
-    return data; // Return the file content
-  });
+// async function readFiles(filePaths) {
+//   // Use map to create an array of promises that read the files
+//   const filePromises = filePaths.map(async path => {
+//     const data = await fs.promises.readFile(path, 'utf-8'); // Read the file
+//     return data; // Return the file content
+//   });
 
-  // Use Promise.all to wait for all file reads to complete
-  const fileContents = await Promise.all(filePromises);
+//   // Use Promise.all to wait for all file reads to complete
+//   const fileContents = await Promise.all(filePromises);
 
-  return fileContents; // This will be an array of the contents of the files
-}
+//   return fileContents; // This will be an array of the contents of the files
+// }
 
-router.get('/', async (req, res => {
-  try {
-    const puzzleFileNames = fs.readdirSync(
-      path.join(__dirname, '../data/puzzles')
-    );
+// router.get('/', async (req, res) => {
+//   try {
+//     const puzzleFileNames = fs.readdirSync(
+//       path.join(__dirname, '../data/puzzles')
+//     );
 
-    const paths = puzzleFileNames.map(file =>
-      path.join(__dirname, `../data/stars/${file}`)
-    );
+//     const paths = puzzleFileNames.map(file =>
+//       path.join(__dirname, `../data/stars/${file}`)
+//     );
 
-    const contents = await readFiles(paths);
+//     const contents = await readFiles(paths);
 
-    res.send(contents.map(json => JSON.parse(json)));
-  } catch (err) {
-    res.status(err.status || 500).send(err);
-  }
-}));
+//     res.send(contents.map(json => JSON.parse(json)));
+//   } catch (err) {
+//     res.status(err.status || 500).send(err);
+//   }
+// });
 
-router.get('/searchByTitle/:title_encoded', async (req, res) => {
-  var { title_encoded } = req.params;
-  try {
-    const response = await searchForMovie(title_encoded);
-    if (!response.ok || !response?.results[0]) next(response);
-    const movie = response.results[0];
+// router.get('/searchByTitle/:title_encoded', async (req, res) => {
+//   var { title_encoded } = req.params;
+//   try {
+//     const response = await searchForMovie(title_encoded);
+//     if (!response.ok || !response?.results[0]) next(response);
+//     const movie = response.results[0];
 
-    const { id, backdrop_path, title, poster_path } = movie;
-    const movieId = 'm_' + id;
+//     const { id, backdrop_path, title, poster_path } = movie;
+//     const movieId = 'm_' + id;
 
-    const movieInfo = { movieId, backdrop_path, title, poster_path };
-    fs.writeFileSync(
-      path.join(__dirname, `../data/movies/${movieId}.json`),
-      JSON.stringify(movieInfo),
-      { flag: 'w' }
-    );
-    res.send(movieInfo);
-  } catch (err) {
-    res.status(err.status || 500).send(err);
-  }
-});
+//     const movieInfo = { movieId, backdrop_path, title, poster_path };
+//     fs.writeFileSync(
+//       path.join(__dirname, `../data/movies/${movieId}.json`),
+//       JSON.stringify(movieInfo),
+//       { flag: 'w' }
+//     );
+//     res.send(movieInfo);
+//   } catch (err) {
+//     res.status(err.status || 500).send(err);
+//   }
+// });
 
-router.get('/:id', async (req, res) => {
-  var { id } = req.params;
-  try {
-    const puzzle = fs.readFileSync(
-      path.join(__dirname, `../data/puzzles/${id}.json`)
-    );
-    res.send(puzzle);
-  } catch (err) {
-    res.status(err.status || 500).send(err);
-  }
-});
+// router.get('/:id', async (req, res) => {
+//   var { id } = req.params;
+//   try {
+//     const puzzle = fs.readFileSync(
+//       path.join(__dirname, `../data/puzzles/${id}.json`)
+//     );
+//     res.send(puzzle);
+//   } catch (err) {
+//     res.status(err.status || 500).send(err);
+//   }
+// });
 
 module.exports = router;
--- routes/stars.js
+++ routes/stars.js
@@ -9,28 +9,28 @@ router.get('/', (req, res) => {
   res.send('Welcome to <> your star api.');
 });
 
-router.get('/searchByName/:name_encoded', async (req, res) => {
-  var { name_encoded } = req.params;
-  try {
-    const response = await searchForStar(name_encoded);
-    if (!response.ok || !response?.results[0]) next(response);
-    const star = response.results[0];
-    var { id, name, profile_path } = star;
-    id = 's_' + id;
-    const text = name;
-    const img = profile_path;
-    const starInfo = { id, img, text };
+// router.get('/searchByName/:name_encoded', async (req, res) => {
+//   var { name_encoded } = req.params;
+//   try {
+//     const response = await searchForStar(name_encoded);
+//     if (!response.ok || !response?.results[0]) next(response);
+//     const star = response.results[0];
+//     var { id, name, profile_path } = star;
+//     id = 's_' + id;
+//     const text = name;
+//     const img = profile_path;
+//     const starInfo = { id, img, text };
 
-    fs.writeFileSync(
-      path.join(__dirname, `../data/stars/${id}.json`),
-      JSON.stringify(starInfo),
-      { flag: 'w' }
-    );
-    res.send(starInfo);
-  } catch (err) {
-    res.status(err.status || 500).send(err);
-  }
-});
+//     fs.writeFileSync(
+//       path.join(__dirname, `../data/stars/${id}.json`),
+//       JSON.stringify(starInfo),
+//       { flag: 'w' }
+//     );
+//     res.send(starInfo);
+//   } catch (err) {
+//     res.status(err.status || 500).send(err);
+//   }
+// });
 
 router.get('/:starId', (req, res) => {});
 
