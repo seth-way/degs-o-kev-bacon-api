4fb660fef65876b4ac327bea9656e977f3abfa0a
Seth Way <way.sethaniel@gmail.com>
2024-09-18 12:50:47 -0400

    debug

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 // };
 
 app.use(cors());
-//app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
