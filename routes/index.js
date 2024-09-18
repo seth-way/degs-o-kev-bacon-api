e8b9db9ed3a0353c0b7c835c8b674fb1b4bd5c97
Seth Way <way.sethaniel@gmail.com>
2024-09-18 13:04:10 -0400

    debug

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-//const moviesRouter = require('./movies.js');
+const moviesRouter = require('./movies.js');
 //const starsRouter = require('./stars.js');
 //const puzzlesRouter = require('./puzzles.js');
 
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
   res.send('Welcome to <> your new api.');
 });
 
-//router.use('/movies', moviesRouter);
+router.use('/movies', moviesRouter);
 //router.use('/stars', starsRouter);
 //router.use('/puzzles', puzzlesRouter);
 
