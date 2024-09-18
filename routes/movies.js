1634ce1a82247732ab588cdda9a5ec0615f658bf
Seth Way <way.sethaniel@gmail.com>
2024-09-18 13:09:39 -0400

    debug

--- routes/movies.js
+++ routes/movies.js
@@ -9,29 +9,32 @@ router.get('/', (req, res) => {
   res.send('Welcome to <> your movies api.');
 });
 
-router.get('/searchByTitle/:title_encoded', async (req, res, next) => {
-  var { title_encoded } = req.params;
-  try {
-    const response = await searchForMovie(title_encoded);
-    if (!response.ok || !response?.results[0]) next(response);
-    const movie = response.results[0];
+router.get(
+  '/searchByTitle/:title_encoded',
+  asyncHandler(async (req, res, next) => {
+    var { title_encoded } = req.params;
+    try {
+      const response = await searchForMovie(title_encoded);
+      if (!response.ok || !response?.results[0]) next(response);
+      const movie = response.results[0];
 
-    var { id, backdrop_path, title, poster_path } = movie;
-    id = 'm_' + id;
-    const img = poster_path;
-    const text = title;
+      var { id, title, poster_path } = movie;
+      id = 'm_' + id;
+      const img = poster_path;
+      const text = title;
 
-    const movieInfo = { id, img, text };
-    fs.writeFileSync(
-      path.join(__dirname, `../data/movies/${id}.json`),
-      JSON.stringify(movieInfo),
-      { flag: 'w' }
-    );
-    res.send(movieInfo);
-  } catch (err) {
-    next(err);
-  }
-});
+      const movieInfo = { id, img, text };
+      fs.writeFileSync(
+        path.join(__dirname, `../data/movies/${id}.json`),
+        JSON.stringify(movieInfo),
+        { flag: 'w' }
+      );
+      res.send(movieInfo);
+    } catch (err) {
+      next(err);
+    }
+  })
+);
 
 router.get('/:movieId', (req, res) => {});
 
