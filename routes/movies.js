f96878dac565c0ce7d46e20d84b6fe7782c48e0d
Seth Way <way.sethaniel@gmail.com>
2024-09-18 13:23:27 -0400

    debug

--- routes/movies.js
+++ routes/movies.js
@@ -2,35 +2,34 @@ const fs = require('fs');
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const asyncHandler = require('express-async-handler');
 const { searchForMovie } = require('../lib/apiCalls');
 
 router.get('/', (req, res) => {
   res.send('Welcome to <> your movies api.');
 });
 
-router.get(
-  '/searchByTitle/:title_encoded',
-  asyncHandler(async (req, res, next) => {
-    var { title_encoded } = req.params;
-    const response = await searchForMovie(title_encoded);
-    if (!response.ok || !response?.results[0]) next(response);
-    const movie = response.results[0];
+router.get('/searchByTitle/:title_encoded', (req, res, next) => {
+  var { title_encoded } = req.params;
+  const movie = searchForMovie(title_encoded)
+    .then(res => {
+      if (!res.ok || !res?.results[0]) next(res);
+      return response.results[0];
+    })
+    .catch(err => next(err));
 
-    var { id, title, poster_path } = movie;
-    id = 'm_' + id;
-    const img = poster_path;
-    const text = title;
+  var { id, title, poster_path } = movie;
+  id = 'm_' + id;
+  const img = poster_path;
+  const text = title;
 
-    const movieInfo = { id, img, text };
-    fs.writeFileSync(
-      path.join(__dirname, `../data/movies/${id}.json`),
-      JSON.stringify(movieInfo),
-      { flag: 'w' }
-    );
-    res.send(movieInfo);
-  })
-);
+  const movieInfo = { id, img, text };
+  fs.writeFileSync(
+    path.join(__dirname, `../data/movies/${id}.json`),
+    JSON.stringify(movieInfo),
+    { flag: 'w' }
+  );
+  res.send(movieInfo);
+});
 
 router.get('/:movieId', (req, res) => {});
 
